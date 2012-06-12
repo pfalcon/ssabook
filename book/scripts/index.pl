35c57f4b2c9bb04bc16916dbc936b70719ae11b6
frastell <frastell@9a4b08f2-1d31-4dde-917c-3c42c574bc2c>
2012-06-12 08:25:50 +0000

    index grep
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/ssabook@537 9a4b08f2-1d31-4dde-917c-3c42c574bc2c

---
+++ book/scripts/index.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+$bookdir = "/home/frastell/ssabook/book/";
+open GREP, "find . -name '*.tex' -exec grep -HE 'index\{[^\\}]+\}' {} \\;|";
+my %indexes = ();
+
+while (my $indexref=<GREP>) {
+    next if ($indexref =~ /\/Springer\//);
+    if ($indexref =~ s/^\.\/(part\d)\/([^\/]+)\/.+://) {
+	($part,$section)=($1,$2);
+    } else { die "pb parsing grep".$indexref; }
+    while ($indexref=~ s/index\{([^\}]+)\}//) {
+	$indexes{$1}{$section}++;
+    }
+}
+
+foreach my $index (sort {uc($a) cmp uc($b)} (keys %indexes)) {
+    print "$index: ", join(" ", keys %{$indexes{$index}}), "\n";
+}
