375fe93b7014e692f0774db8ffb713755bda50a5
fbouchez <fbouchez@9a4b08f2-1d31-4dde-917c-3c42c574bc2c>
2014-02-10 22:00:24 +0000

    [Semantics] finally finished new figures with blocks
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/ssabook@717 9a4b08f2-1d31-4dde-917c-3c42c574bc2c

---
+++ book/part1/semantics/tikz/figure-list.mk
@@ -0,0 +1,12 @@
+FIGS= \
+
+SUBFIGS= \
+  FunctionalCodeCFG-1 \
+  FunctionalCodeCFG-2 \
+  SSAConstructionExample-1 \
+  SSAConstructionExample-2 \
+  SSAConstructionExample-3 \
+  SSAConstructionExample-4 \
+  SSAConstructionExample-5 \
+  SSAConstructionExample-domTree-1 \
+  SSAConstructionExample-domTree-2 \
