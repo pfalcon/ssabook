26651497694da3c28fae9b6f4d5449cc877eba7e
fbouchez <fbouchez@9a4b08f2-1d31-4dde-917c-3c42c574bc2c>
2015-05-07 11:43:19 +0000

    figures for chapter copy propagation
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/ssabook@745 9a4b08f2-1d31-4dde-917c-3c42c574bc2c

--- book/part3/constant_propagation_is_easier/tikz/figure-list.mk
+++ book/part3/constant_propagation_is_easier/tikz/figure-list.mk
@@ -1,4 +1,11 @@
 FIGS= \
-  ssa_graph
 
 SUBFIGS= \
+	 ssa_graph-1 \
+	 ssa_graph-2 \
+	 ssa_propagation-1 \
+	 ssa_propagation-2 \
+	 copy_propagation-1 \
+	 copy_propagation-2 \
+	 copy_propagation_loop-1 \
+	 copy_propagation_loop-2 \
