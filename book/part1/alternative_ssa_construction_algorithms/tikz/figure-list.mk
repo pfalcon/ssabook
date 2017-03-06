ae99314cad3f558d1ce9195d04893fd24b83eca6
fbouchez <fbouchez@9a4b08f2-1d31-4dde-917c-3c42c574bc2c>
2017-03-06 16:05:33 +0000

    [alt] finished updating main figures
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/ssabook@779 9a4b08f2-1d31-4dde-917c-3c42c574bc2c

--- book/part1/alternative_ssa_construction_algorithms/tikz/figure-list.mk
+++ book/part1/alternative_ssa_construction_algorithms/tikz/figure-list.mk
@@ -1,6 +1,13 @@
 FIGS= \
       cfglive \
-      cfgdj
+      cfgdj \
+      irred-lnf \
+      sreedhargao \
+      lnfcfg \
+      lnfred
 
 SUBFIGS= \
+      irred-1 \
+      irred-2 \
+      irred-3 \
 
