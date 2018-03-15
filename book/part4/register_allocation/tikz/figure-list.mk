1c70bcad081bcd9f84a6ce9ba4d49d3326f00a5b
fbouchez <fbouchez@9a4b08f2-1d31-4dde-917c-3c42c574bc2c>
2018-03-15 15:01:06 +0000

    [regalloc] travail sur figures
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/ssabook@822 9a4b08f2-1d31-4dde-917c-3c42c574bc2c

---
+++ book/part4/register_allocation/tikz/figure-list.mk
@@ -0,0 +1,9 @@
+FIGS= \
+      linear-scan \
+      running-ig
+
+SUBFIGS= \
+      running-1 \
+      running-2 \
+      running-3
+
---
+++ book/scripts/init-tikz.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# must be executed in the chapter where tikz needs to be initialized
+
+mkdir -p tikz
+cd tikz
+ln -s ../../../templates/tikz-Makefile Makefile
+ln -s ../../../templates/tikz2figure.tex .
+cp -i ../../../templates/figure-list.mk .
