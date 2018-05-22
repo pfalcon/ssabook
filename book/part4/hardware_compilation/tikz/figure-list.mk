f145feac37259c6783186424e118908e5a608a85
frastell <frastell@9a4b08f2-1d31-4dde-917c-3c42c574bc2c>
2018-05-22 20:26:45 +0000

    figures in hw
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/ssabook@941 9a4b08f2-1d31-4dde-917c-3c42c574bc2c

--- book/part4/hardware_compilation/tikz/figure-list.mk
+++ book/part4/hardware_compilation/tikz/figure-list.mk
@@ -5,7 +5,13 @@ FIGS= \
       fig-4-2 \
       Fig-4-4-a \
       Fig-4-4-b \
-      Fig-4-4-c
+      Fig-4-4-c \
+      Fig-4-5-a \
+      Fig-4-5-b \
+      Fig-4-5-c \
+      Fig-4-6 \
+      Fig-4-7-b \
+      Fig-4-7-c \
 
 SUBFIGS= \
 
