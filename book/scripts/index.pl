daa9863b11e62882972076aa7f410823e5e5a565
frastell <frastell@9a4b08f2-1d31-4dde-917c-3c42c574bc2c>
2018-05-23 23:02:28 +0000

    english+index for alternative destruction
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/ssabook@953 9a4b08f2-1d31-4dde-917c-3c42c574bc2c

--- book/scripts/index.pl
+++ book/scripts/index.pl
@@ -1,9 +1,11 @@
 #!/opt/local/bin/perl
 use re qw(eval);
+use Regexp::Common;
 
 open GREP, "find . -name '*.tex' -exec grep -HE 'index\{.+\}' {} \\;|";
 my %indexes = ();
 my $BALBRACE='(?{local$d=0})(?:\{(?{$d++})|\}(?{$d--})(?(?{$d<0})(?!))|(?>[^\{\}]*))+(?(?{$d!=0})(?!))';
+my $bp = $RE{balanced}{-parens=>'{}'};
 
 
 while (my $indexref=<GREP>) {
@@ -17,12 +19,13 @@ while (my $indexref=<GREP>) {
     if ($indexref =~ s/^\.\/(part\d)\/([^\/]+)\/.+://) {
 	($part,$section)=($1,$2);
     } else { die "pb parsing grep ".$indexref; }
-    while ($indexref=~ s/index\{([^\}]*)\}//) {
+    #    while ($indexref=~ s/index\{([^\}]*)\}//) {
+    while ($indexref=~ s/index($bp)//) {
 	$indexes{$1}{$section}++;
     }
 }
 
 foreach my $index (sort {uc($a) cmp uc($b)} (keys %indexes)) {
-    printf "%-30s:", $index;
+    printf "\\index%-60s:", $index;
     print join(" ", keys %{$indexes{$index}}), "\n";
 }
